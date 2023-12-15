6b098e1f3bb677f86fa54adf73f77f9e7c5e0420
Colin <colin.diesh@gmail.com>
2023-12-15 11:01:25 -0500

    Remove stream-browserify alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  resolve: {
-    alias: {
-      stream: 'stream-browserify',
-    },
-  },
+
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
