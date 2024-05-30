9ff6a7f5cae3ca513cfcfe8170243de663e434cf
Colin <colin.diesh@gmail.com>
2024-05-30 18:56:57 -0400

    Add production sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    sourcemap: true,
+  },
   worker: {
     format: 'es',
   },
