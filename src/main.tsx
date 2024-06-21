a32470b8b198a7ab227640f3dce82557738d99f7
Colin <colin.diesh@gmail.com>
2024-06-21 08:07:02 -0400

    Remove buffer polyfill

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Buffer } from 'buffer'
 
 import './index.css'
-
-window.Buffer = Buffer
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/rpcWorker.ts
+++ src/rpcWorker.ts
@@ -1,5 +1,2 @@
 import '@jbrowse/react-app/esm/workerPolyfill'
 import '@jbrowse/react-app/esm/rpcWorker'
-import { Buffer } from 'buffer'
-
-self.Buffer = Buffer
